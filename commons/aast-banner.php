2ae93bea51318a0cf10345e609fbe456885f7864
rajankz <rajankz@umd.edu>
2011-11-16 10:47:11 -0500

    removed path

--- commons/aast-banner.php
+++ commons/aast-banner.php
@@ -1,1 +1,1 @@
-<a href=<?=$GLOBALS['AAST_ROOT_PATH']?>><img src=<?=$GLOBALS['AAST_ROOT_PATH'] . "/media/images/commons/aast-banner1.jpg" ?>  alt="AAST Banner" height="88px" width="962px" /></a>
+<a href=<?=$GLOBALS['AAST_ROOT_PATH']?>><img src="media/images/commons/aast-banner1.jpg"  alt="AAST Banner" height="88px" width="962px" /></a>
